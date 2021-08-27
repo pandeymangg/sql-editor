bd00d279b2767822f556208e4ce96f10e71d01ae
anshuman9999 <anshuman.pandey9999@gmail.com>
2021-08-27 13:42:09 +0530

    minor bug fix

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import MainQueryResult from "../Layout/MainQueryResult";
 import QuerySidebar from "../Layout/QuerySidebar";
 import { queries } from "../../../data";
+import classes from "./styles.module.css";
 
 const Home = () => {
   const [queriesState, setQueriesState] = useState(queries);
@@ -9,7 +10,7 @@ const Home = () => {
 
   return (
     <div
-      className="lecture_page_layout flex flex-col md:flex-row w-full border-t-2 border-gray-200 dark:border-darkBorder"
+      className={`${classes.mainPage} lecture_page_layout w-full border-t-2 border-gray-200 dark:border-darkBorder`}
       style={{ minHeight: "unset", height: "calc(100vh - 3.5rem)" }}
     >
       <QuerySidebar queries={queriesState} setCurrentQuery={setCurrentQuery} />
